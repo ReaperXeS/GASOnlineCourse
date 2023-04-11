140080cd8fa7d9a2186adf7293665369e00c908f
Reaper XeS <reaperxes@gmail.com>
2023-04-10 21:17:54 -0400

    Section 4.5 Aura Hud to create overlay on begin play.

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Reaper XeS
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OverlayWidget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	OverlayWidget->AddToViewport();
+}
