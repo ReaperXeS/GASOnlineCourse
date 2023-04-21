13b84f57cf245301413a06ea7ea20c6b4ca19635
Reaper XeS <reaperxes@gmail.com>
2023-04-21 15:29:04 -0400

    6.6 Loop on asset tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -10,6 +10,24 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle)
 {
+	// Get tags from effect
+	FGameplayTagContainer Tags;
+	SpecApplied.GetAllAssetTags(Tags);
+
+	// Loop on all tags
+	for (const FGameplayTag& Tag : Tags)
+	{
+		// Check if tag is a message tag
+		// if (Tag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Message"))))
+		// {
+			// Get message from tag
+			const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+
+			// Add on screen message
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Msg);
+		// }
+	}
+	
 	// Add on screen message
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Effect Applied!"));
+	
 }
