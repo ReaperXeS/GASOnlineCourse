4d88feb4b5008fdcc84325582310db682e8d683c
Reaper XeS <reaperxes@gmail.com>
2023-05-26 21:51:26 -0400

    8.18 Attribute info delegate
    Binded in Attributre Menu and TextValueButtonRow

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,9 +3,21 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AttributeInfo.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
 	Super::BroadcastInitialValues();
+
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfo(FAuraGameplayTags::Get().Attributes_Primary_Strength, true);
+	Info.AttributeValue = AuraAttributeSet->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -47,6 +47,7 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParams& InWidgetControllerParams);
 
+	UFUNCTION(BlueprintCallable)
 	virtual void BroadcastInitialValues();
 	virtual void BindCallbacksToDependencies();
 protected:
